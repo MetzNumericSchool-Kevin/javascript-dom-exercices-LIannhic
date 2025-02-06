5ffa938521e638376fcba7dc549c74dbc57f3d66
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-02-06 16:16:12 +0100

    test: afficher uniquement le texte d'un balise sans ces imbrications

---
+++ exercices.js
@@ -0,0 +1,6 @@
+// exercice 01
+
+// Dans une constante récupèré le contenu de la balise h1.
+const element_h1 = document.querySelector("h1");
+// afficher dans la console le texte de la balise h1 sans les balises span avec la propiété textContent
+console.log(element_h1.textContent);
