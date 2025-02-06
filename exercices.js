6c9ff8da42674a013e0cde64a9e9787fae02729b
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-02-06 16:29:10 +0100

    test: supprimer un noed

--- exercices.js
+++ exercices.js
@@ -4,3 +4,14 @@
 const element_h1 = document.querySelector("h1");
 // afficher dans la console le texte de la balise h1 sans les balises span avec la propiété textContent
 console.log(element_h1.textContent);
+
+
+
+// exercice 02
+
+// Dans une constante récupèré le contenu de la balise span avec son id.
+const element_span_h1 = document.querySelector("#blague_de_roger_le_sorcier");
+// Supprimer l'objet span
+element_span_h1.remove();
+// afficher dans la console le texte de la balise h1 sans les balises span avec la propiété textContent
+console.log(element_h1.textContent)
